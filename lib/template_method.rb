1cd1e88dedd0bd8538afcfdff1ea45d5839202b0
asqd <asqd117@gmail.com>
2016-04-11 16:00:33 +0300

    Removing duplicate by inheritance

--- lib/template_method.rb
+++ lib/template_method.rb
@@ -1,20 +1,33 @@
 class Hero
   attr_reader :damage, :abilities
 
-  def initialize(occupation=nil)
-    if occupation == :warrior
-      @damage = 15
-      @abilities = [:strike]
-    elsif occupation == :mage
-      @damage = 7
-      @abilities = [:fireball]
-    else
-      @damage = 10
-      @abilities = []
-    end
+  def initialize
+    @damage = 10
+    @abilities = []
   end
 
   def attack
     "Attack dealing #{damage} damage"
+  end
+
+
+end
+
+class Warrior < Hero
+  attr_reader :damage, :abilities
+
+  def initialize
+    @damage = 15
+    @abilities = [:strike]
+  end
+
+end
+
+class Mage < Hero
+  attr_reader :damage, :abilities
+
+  def initialize
+    @damage = 7
+    @abilities = [:fireball]
   end
 end

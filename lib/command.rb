4df22067d24ba4b1a6c7e0f41ac7d74ed01db9fa
asqd <asqd117@gmail.com>
2016-04-18 15:44:22 +0300

    Commands for robot

---
+++ lib/command.rb
@@ -0,0 +1,23 @@
+class AmplifyShieldCommand
+  def execute
+    "Amplified Plutonium-Gamma Shield"
+  end
+end
+
+class CalibrateDriverCommand
+  def execute
+    "Calibrated Uranium-Rod Driver"
+  end
+end
+
+class TestCompilerCommand
+  def execute
+    "Tested Jupiter Wave Compiler"
+  end
+end
+
+class InstallRegulatorCommand
+  def execute
+    "Installed Hydroelectric Magnetosphere Regulator"
+  end
+end
---
+++ spec/command_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+require_relative '../lib/command'
+
+describe "Command Pattern" do
+
+  describe "Commands" do
+    describe AmplifyShieldCommand do
+      it "amplifies shield" do
+        command = AmplifyShieldCommand.new
+        expect(command.execute).to eq("Amplified Plutonium-Gamma Shield")
+      end
+    end
+
+    describe CalibrateDriverCommand do
+      it "amplifies shield" do
+        command = CalibrateDriverCommand.new
+        expect(command.execute).to eq("Calibrated Uranium-Rod Driver")
+      end
+    end
+
+    describe TestCompilerCommand do
+      it "amplifies shield" do
+        command = TestCompilerCommand.new
+        expect(command.execute).to eq("Tested Jupiter Wave Compiler")
+      end
+    end
+
+    describe InstallRegulatorCommand do
+      it "amplifies shield" do
+        command = InstallRegulatorCommand.new
+        expect(command.execute).to eq("Installed Hydroelectric Magnetosphere Regulator")
+      end
+    end
+  end
+end

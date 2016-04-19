b85261574fefa606c3a3e983aa164f9915c1490e
asqd <asqd117@gmail.com>
2016-04-19 18:01:05 +0300

    Quest composed from tasks

--- spec/composite_spec.rb
+++ spec/composite_spec.rb
@@ -2,6 +2,18 @@ require 'spec_helper'
 require_relative '../lib/composite'
 
 describe 'Composite Pattern' do
+
+  describe Quest do
+    context 'consist from Encounter and Diplomacy' do
+      it 'has reward of 600' do
+        quest = Quest.new
+        quest.add(Encounter.new)
+        quest.add(Diplomacy.new)
+
+        expect(quest.reward).to eq(600)
+      end
+    end
+  end
   describe Encounter do
     it 'has experience reward of 100' do
       expect(Encounter.new.reward).to eq(100)
